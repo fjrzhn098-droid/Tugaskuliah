24971840346a90a3a86525c65f20b7bf9667fc23
fjrzhn098-droid <fjrzhn098@gmail.com>
2025-09-16 14:21:28 +0700

    Add files via upload

---
+++ Tugas kuliah.py
@@ -0,0 +1,13 @@
+from datetime import date
+tgl_v1 = date.today()
+print(tgl_v1)
+
+import datetime as vDate
+hari = vDate.date.day
+
+tanggal = vDate.date.today()
+print(hari)
+print(tanggal)
+
+mahasiswa = ["Fajar", "2024071028","informatika","DAA", tanggal, "Universitas Pembangunan Jaya"]
+print(mahasiswa)
